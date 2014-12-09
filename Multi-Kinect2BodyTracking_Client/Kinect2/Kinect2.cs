ef906eb9121d6281464411fc400e42d7bd3bbad4
Yi-Shiu Chiang <ishiu.chiang@gamil.com>
2014-12-09 15:33:57 +0900

    Singleton Pattern Template added

---
+++ Multi-Kinect2BodyTracking_Client/Kinect2/Kinect2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Patterns.Singleton;
+
+namespace Kinect2
+{
+    class Kinect2 : SingletonBase< Kinect2 >
+    {
+        /// <summary>
+        /// Initializes a new Kinect2 instance of the <see cref="Singleton"/> class.
+        /// </summary>
+        private Kinect2()
+        {
+            Console.WriteLine("Hello Kinect2!");
+        }
+    }
+}
