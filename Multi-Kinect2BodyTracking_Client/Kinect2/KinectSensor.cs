0a6ad3821b44e3b68f2001a368f3436b78ee38bf
Yi-Shiu Chiang <ishiu.chiang@gamil.com>
2014-12-10 15:59:13 +0900

    Add the new members in KinectSensor

--- Multi-Kinect2BodyTracking_Client/Kinect2/KinectSensor.cs
+++ Multi-Kinect2BodyTracking_Client/Kinect2/KinectSensor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using System.Windows.Media.Imaging;
+
 //-- Using Kinect2 API --//
 using Microsoft.Kinect;
 
@@ -15,14 +17,68 @@ namespace Kinect2
         /// <summary>
         /// Active Kinect sensor
         /// </summary>
-        private Microsoft.Kinect.KinectSensor sensor = null;
+        protected Microsoft.Kinect.KinectSensor sensor = Microsoft.Kinect.KinectSensor.GetDefault();
+
+        //----- COLOR STREAM -----//
+        /// <summary>
+        /// Reader for color frames
+        /// </summary>
+        private Microsoft.Kinect.ColorFrameReader colorFrameReader = null;
+
+        /// <summary>
+        /// Bitmap to display
+        /// </summary>
+        private WriteableBitmap colorBitmap = null;
+
+        //----- INFRARED STREAM -----//
+        /// <summary>
+        /// Reader for infrared frames
+        /// </summary>
+        private Microsoft.Kinect.InfraredFrameReader infraredFrameReader = null;
+
+        /// <summary>
+        /// Bitmap to display
+        /// </summary>
+        private WriteableBitmap infraredBitmap = null;
+
+        //----- DEPTH STREAM -----//
+        /// <summary>
+        /// Reader for depth frames
+        /// </summary>
+        private Microsoft.Kinect.DepthFrameReader depthFrameReader = null;
+
+        /// <summary>
+        /// Bitmap to display
+        /// </summary>
+        private WriteableBitmap depthBitmap = null;
+
+        //----- BODY STREAM -----//
+        /// <summary>
+        /// Reader for body frames
+        /// </summary>
+        private Microsoft.Kinect.BodyFrameReader bodyFrameReader = null;
 
         #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
 
         /// <summary>
         /// Initializes a new Kinect2 instance of the <see cref="Singleton"/> class.
         /// </summary>
         private KinectSensor()
         {}
+
+        /// <summary>
+        /// Active the Kinect v2 sensor
+        /// </summary>
+        public void Open() {
+            sensor.Open();
+        }
+
+        #endregion
     }
 }
